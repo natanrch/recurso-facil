700aecccf04dfb1acbbc543b2f11ca4e4af8410d
andre522 <andreluis22.ap@gmail.com>
2018-05-29 17:10:00 -0300

    finalizado cabecalho

---
+++ footer.php
@@ -0,0 +1,30 @@
+<footer>
+        <nav class="bg-dark navbar fixed-bottom ">
+            <div class="container">
+                <div class="row justify-content-start">
+                    <div class="col-md-4">
+                        <h4 class="text-center text-secondary">Sobre</h4>
+                        <p class="text-white">Lorem ipsum dolor sit tempore  itaque dolore error quis cum tempora unde doloribus.</p>
+                    </div>
+                    <div class="col-md-4">
+                        <h4 class="text-center text-secondary">Informações</h4>
+                        <ul class="nav justify-content-center">
+                            <li class="nav-item"><a href="#" class="nav-link text-white">Recurso Facil</a>
+                            <li class="nav-item"><a href="#" class="nav-link text-white">Sobre</a>
+                            <li class="nav-item"><a href="#" class="nav-link text-white">Contato</a>
+                        </ul>
+                    </div>
+                    <div class="col-md-4">
+                        <h4 class="text-secondary text-center">Redes Sociais</h4>
+                        <ul class="nav justify-content-center">
+                            <li class="nav-item"><a href="#" class="nav-link text-white"><i class="fab fa-facebook" id="icon-face"></i></a>
+                            <li class="nav-item"><a href="#" class="nav-link text-white"><i class="fab fa-twitter-square" id="icon-twi"></i></a>
+                            <li class="nav-item"><a href="#" class="nav-link text-white"><i class="fab fa-instagram" id="icon-insta"></i></a>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Usuario</title>
+</head>
+<body>
+    <header class="cabecalho">  
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="cabecalho.php">
+                <img src="./img/logo.png" alt="" class="d-inline-block align-top img-responsive mr-2" width="30" height="30">
+                Recurso Fácil
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Cadastrar <span class="sr-only"></span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Logar</a>
+                </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+            </div>
+        </nav>
+    </header>
