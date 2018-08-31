b8857196cadf1dd40bd16fd60214fadfc8f956ef
Natan Rocha Batista <natan.rocha.batista@gmail.com>
2018-08-30 21:20:35 -0300

    Config.exemplo

---
+++ config.exemplo.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_DRIVE', 'mysql');
+define('DB_HOSTNAME', '127.0.0.1');
+define('DB_DATABASE', 'recurso_facil');
+define('DB_USERNAME', 'usuário');
+define('DB_PASSWORD', 'senha');
